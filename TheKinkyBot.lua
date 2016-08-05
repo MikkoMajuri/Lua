95f8c5a8c51e3fa7dc09e11bb5a858df1068dd73
MikkoMajuri <mikko.majurigmail.com>
2016-08-05 15:56:41 +0300

    forgot the comments

--- TheKinkyBot.lua
+++ TheKinkyBot.lua
@@ -1,4 +1,24 @@
---[[ TheKinkyBot in Telegram - by mikma 2016, http://www.kinky.fi, mikko.majuri@gmail.com ]]
+--[[
+
+TheKinkyBot in Telegram - by mikma 2016, http://www.kinky.fi, mikko.majuri@gmail.com
+
+What config.lua must contain:
+config = {
+        admins = {
+                [123456789] = "Adminname", -- Can also be 'true', just to keep track of names
+        },
+        groups = {
+                [-234567891] = "Group nr. 1", -- Can also be 'true', just to keep track of names
+                [-345678912] = "Group nr. 2", -- Can also be 'true', just to keep track of names
+        },
+        commands = {
+                ["figlet"] = true,
+                ["uptime"] = true,
+        },
+        token = "456789123:ABCdefgHIjKlmn_aBcD1_e2Fgh3iJ45KlMN",
+}
+
+]]
 
 -- debug purposes - https://github.com/kikito/inspect.lua.git
 local inspect = require('inspect')
